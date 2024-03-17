af765e4eb07d41607822b5adecf0525ae1e2438f
wheresteresa <64266886+wheresteresa@users.noreply.github.com>
2024-03-17 17:56:51 +0000

    Update Penguins.py

--- Penguins.py
+++ Penguins.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import pandas as pd
 import plotly.express as px
@@ -10,20 +8,16 @@ import plotly.graph_objects as go
 
 df=pd.read_csv("penguins.csv")
 df=df.dropna()
-df
 
 
-# In[2]:
-
-
-# 颜色映射
+# Color definition
 color_discrete_map = {
     'Adelie': '#27005D',
     'Gentoo': '#9400FF',
     'Chinstrap': '#8594E7'
 }
 
-# 创建初始图表
+# Create the basic chart
 fig = px.scatter(df, x="bill_length_mm", y="body_mass_g",
                  color="species",
               
@@ -43,12 +37,12 @@ fig.update_layout(
     paper_bgcolor='#FCFBF9',
     title_font=dict(size=24))
 
-# 初始化轨迹可见性列表
-initial_visibility = [True] * len(fig.data)  # 初始所有物种可见
+# Initialize the track visibility list
+initial_visibility = [True] * len(fig.data)  # all species are visible initially 
 
-# 添加性别区分的轨迹
-all_traces_len = len(fig.data)  # 记录初始轨迹数量
-for sex in df['sex'].dropna().unique():  # 排除缺失性别的数据
+# Add gender-specific trajectories
+all_traces_len = len(fig.data)
+for sex in df['sex'].dropna().unique():
     for species in df['species'].unique():
         df_filtered = df[(df['sex'] == sex) & (df['species'] == species)]
         fig.add_trace(go.Scatter(
@@ -57,27 +51,27 @@ for sex in df['sex'].dropna().unique():  # 排除缺失性别的数据
             mode='markers',
             name=f"{species} ({sex})",
             marker=dict(color=color_discrete_map[species]),
-            visible=False  # 初始不显示
+            visible=False 
         ))
 
-# 定义按钮逻辑
+# Define the function of button
 buttons = [
     dict(label="All",
          method="update",
-         args=[{"visible": initial_visibility + [False] * (len(fig.data) - all_traces_len)},  # 只显示初始物种轨迹
+         args=[{"visible": initial_visibility + [False] * (len(fig.data) - all_traces_len)},
                {"title": "Bill Length & Body Mass by Species"}]),
 ]
 
-# 分别为男性和女性添加按钮
+# Add buttons
 for sex in df['sex'].dropna().unique():
-    visibility = [False] * all_traces_len  # 初始轨迹隐藏
-    visibility += [(t.name.endswith(f"({sex})")) for t in fig.data[all_traces_len:]]  # 根据性别显示轨迹
+    visibility = [False] * all_traces_len 
+    visibility += [(t.name.endswith(f"({sex})")) for t in fig.data[all_traces_len:]] 
     buttons.append(dict(label=sex,
                         method="update",
                         args=[{"visible": visibility},
                               {"title": f"Bill Length & Body Mass by Species: {sex}"}]))
 
-# 更新图表布局以添加按钮
+# Update the layout of the chart
 fig.update_layout(updatemenus=[dict(type="buttons", direction="down", x=1.115, y=0.8, buttons=buttons)])
 
 fig.update_xaxes(title_text='Bill Length (mm)',gridcolor='#EEEEEE')
@@ -88,7 +82,6 @@ fig.update_traces(marker=dict(size=10))
 fig.show()
 
 
-# In[ ]:
 
 
 
